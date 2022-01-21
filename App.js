a39359f8c9c53a549b6de9fe8dd20ad6c1be6f47
Moabe <moabeluis2018@gmail.com>
2022-01-20 21:37:06 -0300

    Criando a logica de permissão da camera

--- App.js
+++ App.js
@@ -6,6 +6,22 @@ import { Camera } from 'expo-camera';
 export default function App() {
 
   const [type, setType] = useState(Camera.Constants.Type.back)
+  const [hasPermission , setHasPermission] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync()
+      setHasPermission(status === "granted")
+    })()
+  }, [])
+
+  if(hasPermission == null){
+    return <View/>
+  }
+
+  if(hasPermission === false){
+    return <Text>Acesso negado</Text>
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -24,6 +40,6 @@ const styles = StyleSheet.create({
   },
   camera: {
     width: "100%",
-    height: "100%"
+    height: "100%",
   }
 });
